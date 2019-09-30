081d22667cc1217eaf391b2d300ee739a30e1052
ProteanBear <moru_1982@hotmail.com>
2019-09-30 09:43:28 +0800

    Add react project with antd using typescript

---
+++ empty-react-antd-ts/src/app/Router.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {HashRouter as Router,Route, Switch} from 'react-router-dom';
+
+/**
+ * 顶层路由
+ */
+export default class AppRouter extends React.Component{
+    render(){
+        return (
+            <Router>
+                <Switch>
+                    
+                </Switch>
+            </Router>
+        );
+    }
+}
